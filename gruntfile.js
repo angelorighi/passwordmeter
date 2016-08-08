3c540dd8ff5a3acb211011cf917293425a62242e
Angelo Righi <angelo.righi@gmail.com>
2016-08-08 14:11:16 +0200

    Added Grunt

---
+++ gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+      qunit: {
+        all: ['test/**/*_test.html']
+      },
+
+      jsdoc : {
+          dist : {
+              src: ['*.js'],
+              options: {
+                  destination: 'doc'
+              }
+          }
+      }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-jsdoc');
+
+};
